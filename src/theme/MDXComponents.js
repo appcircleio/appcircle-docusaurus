bf9bcbf4cc0b19d3ced606b4f4909ec2baa4a0c6
Furkan Arabacı <furkan.arabaci@smartface.io>
2023-11-18 16:20:05 +0300

    feature(BE-2186): Add disclaimer

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -6,6 +6,16 @@ import NarrowImage from '@site/src/components/NarrowImage';
 import ContentRef from '@site/src/components/ContentRef';
 import ExternalUrlRef from '@site/src/components/ExternalUrlRef';
 
+/**
+ * Note to contributor:
+ * The components that are added here are are registered as Global Export on Docusaurus/MDX like Link, CodeBlock etc.
+ * This means, these will be imported as overhead which can slow down first load or build time.
+ * Therefore, here should ONLY contain components which will be used all across the documentation, frequently.
+ * 
+ * If your component is only used on a single documentation, DO NOT register them as global export; instead, stick to local import.
+ * For example, there's SelfHostedBadge component which is only used on release notes. DO NOT add that or similar components here.
+ */
+
 export default {
   ...MDXComponents, // Re-use the default mapping
   Screenshot,
