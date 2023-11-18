4276bf0574d7a81dffda0949df70628e0d59dddf
Furkan Arabacı <furkan.arabaci@smartface.io>
2023-11-18 16:10:29 +0300

    feature(BE-2186): Add background for Global Imports

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+
+import Screenshot from '@site/src/components/Screenshot';
+import NarrowImage from '@site/src/components/NarrowImage';
+import ContentRef from '@site/src/components/ContentRef';
+import ExternalUrlRef from '@site/src/components/ExternalUrlRef';
+
+export default {
+  ...MDXComponents, // Re-use the default mapping
+  Screenshot,
+  NarrowImage,
+  ContentRef,
+  ExternalUrlRef
+};
