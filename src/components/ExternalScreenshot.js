0fabc88c15e1d106c0a84eca4beb76c6be746882
Onur Yılmaz <csonuryilmaz@gmail.com>
2023-11-28 06:57:22 +0300

    It should only be used for remote URL

--- src/components/ExternalScreenshot.js
+++ src/components/ExternalScreenshot.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 /**
- * The urls are for remote content. For local content, please do not use this component.
- * If you really want to use it, you might use it like `require('../path/to/screenshot')`
- * @param param0 
+ * The URLs are for remote content. For local content, please do not use this component.
+ *
  * @returns 
  */
 export default function Screenshot({ url, width = 0, height = 0 }) {
