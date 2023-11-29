c0747af16f3bcc667b4c07edb1473c13862243bb
Furkan Arabacı <furkan.arabaci@smartface.io>
2023-11-29 11:40:14 +0300

    fix(BE-2198): Add min condition for maxHeight

--- src/components/ExternalScreenshot.js
+++ src/components/ExternalScreenshot.js
@@ -10,13 +10,14 @@ export default function ExternalScreenshot({ url, width = 0, height = 0, maxHeig
   const widthNumber = parseInt(width);
   const heightNumber = parseInt(height);
   const aspectRatio = widthNumber / heightNumber;
-  const calculatedMaxHeight = 898 / (aspectRatio); 
+  const calculatedMaxHeight = Math.min(650, 898 / (aspectRatio));
   /**
    * Here's what's going on here:
    * Since we provide the width height as pixels, we first extract the 'px' part. E.g. '3090px' becomes 3090.
    * Then, we calculate the aspect ratio by dividing width to height.
    * Then, We calculate the necessary maxHeight to the maxWidth which is given by docusaurus.
    * 898 is the maxWidth of the parent. 930 is the maximum width and 32 is with margins of 16px from each side. 
+   * 650 is the default maxHeight we provide for images.
    */
   return (
     <img className="external-screenshot" src={url} width={width} height={height} style={{ maxHeight: maxHeight || calculatedMaxHeight }}></img>
