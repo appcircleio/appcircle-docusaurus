002f37e516f947ce125e70ba95e3edb808d47a39
erenkan <erenkn@gmail.com>
2024-07-12 15:39:20 +0300

    test

--- src/plugins/remark-externalUrlRef.js
+++ src/plugins/remark-externalUrlRef.js
@@ -4,40 +4,20 @@ import isUrl from "is-url";
 
 const remarkExternalUrlRef = () => {
   return async (tree) => {
-    const nodesToTransform = [];
+    const textNodesToTransform = [];
 
     visit(tree, "text", (node, index, parent) => {
-      if (!parent || parent.type !== "paragraph") return;
+      if (!parent) return; // Skip if no parent is found
 
-      const words = node.value.split(/(\s+)/).filter(Boolean); // Preserve spaces as separate nodes
-      words.forEach((word, wordIndex) => {
-        if (isUrl(word)) {
-          nodesToTransform.push({ parent, node, index, word, wordIndex });
-        }
-      });
+      const urls = node.value.split(/\s+/).filter((word) => isUrl(word));
+      if (urls.length > 0) {
+        textNodesToTransform.push({ parent, index, urls });
+      }
     });
 
-    for (let { parent, node, index, word, wordIndex } of nodesToTransform) {
-      const surroundingText = node.value.split(word);
-      const newNodes = await transformNode(word);
-
-      const beforeTextNode = {
-        type: "text",
-        value: surroundingText[0],
-      };
-
-      const afterTextNode = {
-        type: "text",
-        value: surroundingText[1],
-      };
-
-      parent.children.splice(
-        index,
-        1,
-        beforeTextNode,
-        ...newNodes,
-        afterTextNode
-      );
+    for (let { parent, index, urls } of textNodesToTransform) {
+      const newNodes = await Promise.all(urls.map((url) => transformNode(url)));
+      parent.children.splice(index, 1, ...newNodes);
     }
 
     return tree;
@@ -46,30 +26,30 @@ const remarkExternalUrlRef = () => {
 
 const transformNode = async (url) => {
   const data = await fetchData(url);
-  return [
-    {
-      type: "mdxJsxFlowElement",
-      name: "ExternalUrlRef",
-      attributes: [
-        { type: "mdxJsxAttribute", name: "url", value: url },
-        { type: "mdxJsxAttribute", name: "title", value: data.title },
-        {
-          type: "mdxJsxAttribute",
-          name: "description",
-          value: data.description || "No description available.",
-        },
-        {
-          type: "mdxJsxAttribute",
-          name: "image",
-          value:
-            data.image ||
-            "https://cdn.appcircle.io/docs/assets/appcircle-logo.png",
-        },
-      ],
-      children: [{ type: "text", value: url }],
-      data: { _mdxExplicitJsx: true },
+  return {
+    type: "mdxJsxFlowElement",
+    name: "ExternalUrlRef",
+    attributes: [
+      { type: "mdxJsxAttribute", name: "url", value: url },
+      { type: "mdxJsxAttribute", name: "title", value: data.title },
+      {
+        type: "mdxJsxAttribute",
+        name: "description",
+        value: data.description || "No description available.",
+      },
+      {
+        type: "mdxJsxAttribute",
+        name: "image",
+        value:
+          data.image ||
+          "https://cdn.appcircle.io/docs/assets/appcircle-logo.png",
+      },
+    ],
+    children: [],
+    data: {
+      _mdxExplicitJsx: true,
     },
-  ];
+  };
 };
 
 const fetchData = async (url) => {
@@ -84,19 +64,21 @@ const fetchData = async (url) => {
     const { result } = await ogs(options);
     if (result.success) {
       const title = result.ogTitle || new URL(url).hostname;
-      const description = result.ogDescription || "No description available";
+      const description = result.ogDescription || "No description available"; // Default description
       const image =
-        result.ogImage?.url ||
-        "https://cdn.appcircle.io/docs/assets/appcircle-logo.png";
+        result.ogImage && result.ogImage.url
+          ? result.ogImage.url
+          : "https://cdn.appcircle.io/docs/assets/appcircle-logo.png"; // Default image
       return { title, description, image };
     } else {
       throw new Error("OGS failed to fetch the title");
     }
   } catch (error) {
+    // console.error(`Error fetching data for URL ${url}: ${error}`); // Commented out to prevent spamming the console
     return {
       title: new URL(url).hostname,
-      description: "No description available",
-      image: "https://cdn.appcircle.io/docs/assets/appcircle-logo.png",
+      description: "No description available", // Default description
+      image: "https://cdn.appcircle.io/docs/assets/appcircle-logo.png", // Default image
     };
   }
 };
